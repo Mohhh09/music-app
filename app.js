bc1c41baee3d0538d7fd2d9568f445c2d39d4289
abiose Mohammed <abiosemohammed01@gmail.com>
2023-04-19 17:07:17 +0100

    first commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // get elements from the DOM
-const audio = document.querySelector('audio');
+// const audio = document.querySelector('audio');
 const seekSlider = document.querySelector('.seek_slider');
 const volumeSlider = document.querySelector('.volume_slider');
 const currentTime = document.querySelector('.current-time');
@@ -10,6 +10,9 @@ const trackName = document.querySelector('.track-name');
 const trackArtist = document.querySelector('.track-artist');
 const trackArt = document.querySelector('.track-art img');
 const nowPlaying = document.querySelector('.nowplaying');
+const audio = document.getElementById('song');
+const sources = Array.from(audio.querySelectorAll('source')).map(source => source.src);
+const songs = audio.getElementsByTagName('source');
 
 // initialize audio duration and current time
 audio.onloadedmetadata = () => {
@@ -49,5 +52,57 @@ function formatTime(time) {
   return `${Math.floor(time / 60)}:${(Math.floor(time % 60) < 10) ? '0' : ''}${Math.floor(time % 60)}`;
 }
 
+// Keep track of the current song index
+let currentSongIndex = 0;
+
+// Function to skip to the next song
+function nextTrack() {
+  // Increment the current song index
+  currentSongIndex++;
+  
+  // If the current song index is greater than or equal to the number of songs, wrap around to the beginning
+  if (currentSongIndex >= sources.length) {
+    currentSongIndex = 0;
+  }
+  
+  // Set the new source URL and play the audio
+  audio.src = sources[currentSongIndex];
+  audio.play();
+  
+  // Update the track name and artist in the UI
+  const trackNameElement = document.querySelector('.track-name');
+  const trackArtistElement = document.querySelector('.track-artist');
+  const currentSource = sources[currentSongIndex];
+  const trackName = currentSource.substring(currentSource.lastIndexOf('/') + 1, currentSource.lastIndexOf('.mp3')).trim();
+  const trackArtist = 'Kendrick Lamar'; 
+  trackNameElement.textContent = trackName;
+  trackArtistElement.textContent = trackArtist;
+}
+
+// Call the nextTrack function when the "Next" button is clicked
+const nextButton = document.querySelector('.next-track');
+nextButton.addEventListener('click', nextTrack);
+
+function playSong(index) {
+    audio.pause();
+    audio.currentTime = 0;
+    audio.src = songs[index].src;
+    audio.load();
+    audio.play();
+    currentSongIndex = index;
+  }
+  
+  function nextSong() {
+    const nextIndex = (currentSongIndex + 1) % songs.length;
+    playSong(nextIndex);
+  }
+  
+  function prevSong() {
+    const prevIndex = (currentSongIndex + songs.length - 1) % songs.length;
+    playSong(prevIndex);
+  }
+  
+  document.querySelector('.prev-track').addEventListener('click', prevSong);
+  document.querySelector('.next-track').addEventListener('click', nextSong);
 
 
